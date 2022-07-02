656d5a5f977c43d8058ef85e042397cfbca051c3
Pedro Manoel <pedro.alves@ccc.ufcg.edu.br>
2022-07-02 19:28:19 -0300

    feat: create-user-controller is complete (#5)

--- src/modules/users/use-cases/create-user/create-user-controller.ts
+++ src/modules/users/use-cases/create-user/create-user-controller.ts
@@ -1,19 +1,29 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
+import User from '@/modules/users/entities/user';
 import HTTPResponse from '@/shared/infra/http/models/http-response';
 import UseCaseController from '@/shared/use-cases/use-case-controller';
 
 import CreateUserUseCase from './create-user-use-case';
 
 class CreateUserController implements UseCaseController {
-  handle = async (_request: Request, response: Response): Promise<Response> => {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, email, password, phoneNumber } = request.body;
+
     const createUserUseCase = container.resolve(CreateUserUseCase);
 
-    const user = await createUserUseCase.execute();
+    const user = await createUserUseCase.execute({
+      name,
+      email,
+      password,
+      phoneNumber,
+    });
 
-    return new HTTPResponse(response).ok(user);
-  };
+    const userJson = User.toJson(user);
+
+    return new HTTPResponse(response).created(userJson);
+  }
 }
 
 export default CreateUserController;
