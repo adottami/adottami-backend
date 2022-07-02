9223ffe9c992ecf991d21dfe806c97b23dd5ca7d
Pedro Manoel <pedro.alves@ccc.ufcg.edu.br>
2022-07-02 19:24:30 -0300

    test: create-user-use-case-test is complete (#5)

--- src/modules/users/use-cases/create-user/__tests__/create-user-use-case.test.ts
+++ src/modules/users/use-cases/create-user/__tests__/create-user-use-case.test.ts
@@ -1,12 +1,71 @@
 import UserRepositoryMock from '@/modules/repositories/mocks/user-repository-mock';
+import UserRepository from '@/modules/repositories/user-repository';
+import User from '@/modules/users/entities/user';
+import HashProvider from '@/shared/container/providers/hash-provider/hash-provider';
+import HashProviderMock from '@/shared/container/providers/hash-provider/mocks/hash-provider-mock';
+import BadRequestHTTPError from '@/shared/infra/http/errors/bad-request-http-error';
 
 import CreateUserUseCase from '../create-user-use-case';
 
 describe('Create user controller', () => {
-  const userRepository = new UserRepositoryMock();
+  let userRepository: UserRepository;
+  let hashProvider: HashProvider;
+  let useCase: CreateUserUseCase;
 
-  it('should ...', () => {
-    const useCase = new CreateUserUseCase(userRepository); // eslint-disable-line @typescript-eslint/no-unused-vars
-    // ...
+  beforeEach(() => {
+    userRepository = new UserRepositoryMock();
+    hashProvider = new HashProviderMock();
+    useCase = new CreateUserUseCase(userRepository, hashProvider);
+  });
+
+  it('should be able to create a new user', async () => {
+    const userData = {
+      name: 'Test name',
+      email: 'test@test.com.br',
+      password: '1234',
+      phoneNumber: '123456789',
+    };
+
+    const user = await useCase.execute(userData);
+
+    expect(user).toHaveProperty('id');
+    expect(user).toBeInstanceOf(User);
+  });
+
+  it('should be able to create a new user with encrypted password', async () => {
+    const userData = {
+      name: 'Test name',
+      email: 'test@test.com.br',
+      password: '1234',
+      phoneNumber: '123456789',
+    };
+
+    const user = await useCase.execute(userData);
+
+    const samePasswords = await hashProvider.compare(userData.password, user.password);
+
+    expect(user).toHaveProperty('id');
+    expect(userData.password === user.password).toBeFalsy();
+    expect(samePasswords).toBeTruthy();
+  });
+
+  it('should not be able to create a new user with email already registered', async () => {
+    const userData1 = {
+      name: 'Test name',
+      email: 'test@test.com.br',
+      password: '1234',
+      phoneNumber: '123456789',
+    };
+
+    const userData2 = {
+      name: 'Test name 2',
+      email: userData1.email,
+      password: '1288',
+      phoneNumber: '123456789',
+    };
+
+    await useCase.execute(userData1);
+
+    await expect(useCase.execute(userData2)).rejects.toEqual(new BadRequestHTTPError('User already exists'));
   });
 });
