da2c2df68f983b5404f213480fe2be9bb4b86a8f
Pedro Manoel <pedro.alves@ccc.ufcg.edu.br>
2022-07-03 20:39:23 -0300

    refactor: creating static attribute in http-response for status code representation (#5)

--- src/shared/infra/http/models/http-response.ts
+++ src/shared/infra/http/models/http-response.ts
@@ -1,42 +1,54 @@
 import { Response } from 'express';
 
 class HTTPResponse {
+  static STATUS_CODE = Object.freeze({
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    CONFLICT: 409,
+    INTERNAL_SERVER_ERROR: 500,
+  });
+
   constructor(private response: Response) {}
 
   ok<ResponseBody>(body?: ResponseBody): Response {
-    return this.response.status(200).json(body);
+    return this.response.status(HTTPResponse.STATUS_CODE.OK).json(body);
   }
 
   created<ResponseBody>(body?: ResponseBody): Response {
-    return this.response.status(201).json(body);
+    return this.response.status(HTTPResponse.STATUS_CODE.CREATED).json(body);
   }
 
   noContent(): Response {
-    return this.response.status(204).send();
+    return this.response.status(HTTPResponse.STATUS_CODE.NO_CONTENT).send();
   }
 
   badRequest<ResponseBody>(body?: ResponseBody): Response {
-    return this.response.status(400).json(body);
+    return this.response.status(HTTPResponse.STATUS_CODE.BAD_REQUEST).json(body);
   }
 
   unauthorized<ResponseBody>(body?: ResponseBody): Response {
-    return this.response.status(401).json(body);
+    return this.response.status(HTTPResponse.STATUS_CODE.UNAUTHORIZED).json(body);
   }
 
   forbidden<ResponseBody>(body?: ResponseBody): Response {
-    return this.response.status(403).json(body);
+    return this.response.status(HTTPResponse.STATUS_CODE.FORBIDDEN).json(body);
   }
 
   notFound<ResponseBody>(body?: ResponseBody): Response {
-    return this.response.status(404).json(body);
+    return this.response.status(HTTPResponse.STATUS_CODE.NOT_FOUND).json(body);
   }
 
   conflict<ResponseBody>(body?: ResponseBody): Response {
-    return this.response.status(409).json(body);
+    return this.response.status(HTTPResponse.STATUS_CODE.CONFLICT).json(body);
   }
 
   internalServerError<ResponseBody>(body?: ResponseBody): Response {
-    return this.response.status(500).json(body);
+    return this.response.status(HTTPResponse.STATUS_CODE.INTERNAL_SERVER_ERROR).json(body);
   }
 }
 
