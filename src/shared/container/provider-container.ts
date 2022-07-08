9f6d3ee74e7b20c63be247e7005ba1dd853876f6
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-07-08 10:10:23 -0300

    refactor: abstract class type utility

--- src/shared/container/provider-container.ts
+++ src/shared/container/provider-container.ts
@@ -3,14 +3,13 @@ import { container } from 'tsyringe';
 import globalConfig from '@/config/global-config/global-config';
 import HashProvider from '@/shared/container/providers/hash-provider/hash-provider';
 import BcryptHashProvider from '@/shared/container/providers/hash-provider/implementations/bcrypt-hash-provider/bcrypt-hash-provider';
+import { ClassType } from '@/types/utils';
 
 import CloudinaryStorageProvider from './providers/storage-provider/implementations/cloudinary-storage-provider/cloudinary-storage-provider';
 import LocalStorageProvider from './providers/storage-provider/implementations/local-storage-provider/local-storage-provider';
 import StorageProvider from './providers/storage-provider/storage-provider';
 
-type StorageProviderClass = new (...parameters: unknown[]) => StorageProvider;
-
-function getActiveStorageProvider(): StorageProviderClass {
+function getActiveStorageProvider(): ClassType<StorageProvider> {
   return globalConfig.mode() === 'production' ? CloudinaryStorageProvider : LocalStorageProvider;
 }
 
