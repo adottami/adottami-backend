ab07b857d3c6a0b6f3a2055bb1f77979cd4849f7
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-07-08 09:23:38 -0300

    feat: create local storage provider

--- src/shared/container/provider-container.ts
+++ src/shared/container/provider-container.ts
@@ -1,12 +1,20 @@
 import { container } from 'tsyringe';
 
+import globalConfig from '@/config/global-config/global-config';
 import HashProvider from '@/shared/container/providers/hash-provider/hash-provider';
 import BcryptHashProvider from '@/shared/container/providers/hash-provider/implementations/bcrypt-hash-provider/bcrypt-hash-provider';
 
 import CloudinaryStorageProvider from './providers/storage-provider/implementations/cloudinary-storage-provider/cloudinary-storage-provider';
+import LocalStorageProvider from './providers/storage-provider/implementations/local-storage-provider/local-storage-provider';
 import StorageProvider from './providers/storage-provider/storage-provider';
+
+type StorageProviderClass = new (...parameters: unknown[]) => StorageProvider;
+
+function getActiveStorageProvider(): StorageProviderClass {
+  return globalConfig.mode() === 'production' ? CloudinaryStorageProvider : LocalStorageProvider;
+}
 
 export function registerProviderSingletons() {
   container.registerSingleton<HashProvider>('HashProvider', BcryptHashProvider);
-  container.registerSingleton<StorageProvider>('StorageProvider', CloudinaryStorageProvider);
+  container.registerSingleton<StorageProvider>('StorageProvider', getActiveStorageProvider());
 }
---
+++ src/shared/container/providers/storage-provider/implementations/local-storage-provider/constants.ts
@@ -0,0 +1,5 @@
+import path from 'path';
+
+import { PROJECT_ROOT_DIRECTORY } from '@/config/global-config/constants';
+
+export const LOCAL_STORAGE_FOLDER = path.join(PROJECT_ROOT_DIRECTORY, 'tmp', 'uploads');
