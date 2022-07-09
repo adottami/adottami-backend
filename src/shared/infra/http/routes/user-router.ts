411c75206dbcccb3055b5a74f264ee0871621f17
Pedro Manoel <pedro.alves@ccc.ufcg.edu.br>
2022-07-09 17:55:48 -0300

    feat: add get user router (#13)

--- src/shared/infra/http/routes/user-router.ts
+++ src/shared/infra/http/routes/user-router.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
 import CreateUserController from '@/modules/users/use-cases/create-user/create-user-controller';
+import GetUserController from '@/modules/users/use-cases/get-user/get-user-controller';
 
 const userRouter = Router();
 
 const createUserController = new CreateUserController();
 userRouter.post('/', createUserController.handle);
+
+const getUserController = new GetUserController();
+userRouter.get('/:id', getUserController.handle);
 
 export default userRouter;
