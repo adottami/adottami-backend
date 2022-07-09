744ebc67b4383dd3d22e828a9d4c197e67cf4d3c
Pedro Manoel <pedro.alves@ccc.ufcg.edu.br>
2022-07-09 20:24:53 -0300

    feat: add authentications in the route (#13)

--- src/shared/infra/http/routes/user-router.ts
+++ src/shared/infra/http/routes/user-router.ts
@@ -3,12 +3,15 @@ import { Router } from 'express';
 import CreateUserController from '@/modules/users/use-cases/create-user/create-user-controller';
 import GetUserController from '@/modules/users/use-cases/get-user/get-user-controller';
 
+import ensureAuthenticated from '../middlewares/ensure-authenticated';
+import ensureConfirmUser from '../middlewares/ensure-confirm-user';
+
 const userRouter = Router();
 
 const createUserController = new CreateUserController();
 userRouter.post('/', createUserController.handle);
 
 const getUserController = new GetUserController();
-userRouter.get('/:id', getUserController.handle);
+userRouter.get('/:id', ensureAuthenticated, ensureConfirmUser, getUserController.handle);
 
 export default userRouter;
