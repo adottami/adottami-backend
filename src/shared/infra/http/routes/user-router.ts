e7e28e37b41d8cd4139455b3653a5c23f5d1caec
Pedro Manoel <pedro.alves@ccc.ufcg.edu.br>
2022-07-10 15:53:13 -0300

    feat: add functionality and its tests (#8)

--- src/shared/infra/http/routes/user-router.ts
+++ src/shared/infra/http/routes/user-router.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 
 import CreateUserController from '@/modules/users/use-cases/create-user/create-user-controller';
+import UpdateUserController from '@/modules/users/use-cases/update-user/update-user-controller';
+
+import ensureAuthenticated from '../middlewares/ensure-authenticated';
+import ensureConfirmUser from '../middlewares/ensure-confirm-user';
 
 const userRouter = Router();
 
 const createUserController = new CreateUserController();
 userRouter.post('/', createUserController.handle);
+
+const updateUserController = new UpdateUserController();
+userRouter.post('/', ensureAuthenticated, ensureConfirmUser, updateUserController.handle);
 
 export default userRouter;
