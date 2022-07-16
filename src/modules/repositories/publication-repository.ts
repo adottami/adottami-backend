dd9187346d69a83bb46c807321782dcebd547225
guilhermefpeixoto <guilherme.peixoto@ccc.ufcg.edu.br>
2022-07-15 21:36:43 -0300

    feat: create get publication use case and controller

--- src/modules/repositories/publication-repository.ts
+++ src/modules/repositories/publication-repository.ts
@@ -2,6 +2,7 @@ import Publication from '@/modules/publications/entities/publication';
 
 interface PublicationRepository {
   create(authorId: string, publication: Publication): Promise<Publication>;
+  findById(id: string): Promise<Publication | null>;
 }
 
 export default PublicationRepository;
