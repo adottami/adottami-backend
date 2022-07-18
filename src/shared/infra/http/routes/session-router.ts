69e0ac12682eb4d2608472d6bd4f625938faf63c
pedrohenrique-ql <pedrohenrique.dev@gmail.com>
2022-07-18 13:29:47 -0300

    feat: create logout route

--- src/shared/infra/http/routes/session-router.ts
+++ src/shared/infra/http/routes/session-router.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 
 import AccessTokenController from '@/modules/sessions/use-cases/access-token/access-token-controller';
 import LoginSessionController from '@/modules/sessions/use-cases/login/login-session-controller';
+import LogoutSessionController from '@/modules/sessions/use-cases/logout/logout-session-controller';
+
+import ensureAuthenticated from '../middlewares/ensure-authenticated';
 
 const sessionRouter = Router();
 
@@ -10,5 +13,8 @@ sessionRouter.post('/login', loginSessionController.handle);
 
 const accessTokenController = new AccessTokenController();
 sessionRouter.post('/access-token', accessTokenController.handle);
+
+const logoutSessionController = new LogoutSessionController();
+sessionRouter.post('/logout', ensureAuthenticated, logoutSessionController.handle);
 
 export default sessionRouter;
