3943965312ec3549a9df59ca73ebb453864b2724
guilhermefpeixoto <guilherme.peixoto@ccc.ufcg.edu.br>
2022-07-23 18:33:58 -0300

    feat: create update publication route

--- src/shared/infra/http/routes/publication-router.ts
+++ src/shared/infra/http/routes/publication-router.ts
@@ -4,8 +4,10 @@ import CreatePublicationController from '@/modules/publications/use-cases/create
 import GetPublicationController from '@/modules/publications/use-cases/get-publication/get-publication-controller';
 import GetPublicationsController from '@/modules/publications/use-cases/get-publications/get-publications-controller';
 import ListCharacterisctsController from '@/modules/publications/use-cases/list-characteristics/list-characteristics-controller';
+import UpdatePublicationController from '@/modules/publications/use-cases/update-publication/update-publication-controller';
 
 import ensureAuthenticated from '../middlewares/ensure-authenticated';
+import ensureConfirmUser from '../middlewares/ensure-confirm-user';
 
 const publicationRouter = Router();
 
@@ -20,5 +22,8 @@ publicationRouter.get('/', getPublicationsController.handle);
 
 const getPublicationController = new GetPublicationController();
 publicationRouter.get('/:id', ensureAuthenticated, getPublicationController.handle);
+
+const updatePublicationController = new UpdatePublicationController();
+publicationRouter.patch('/:id', ensureAuthenticated, ensureConfirmUser, updatePublicationController.handle);
 
 export default publicationRouter;
